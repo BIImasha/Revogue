4cf0cd319f083a45913598239154c7d314eefb87
B. I. Imasha <10953702@students.plymouth.ac.uk>
2026-05-18 00:12:07 +0530

    add avif to allowed cloudinary formats

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -18,7 +18,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "revogue",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    allowed_formats: ["jpg", "jpeg", "png", "webp", "avif"],
   },
 });
 
